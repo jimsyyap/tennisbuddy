ca309348d4c8850451a5be66518ebb7a5d2da98f
jimmy desu <jimsyyap@gmail.com>
2024-08-25 17:00:59 +1000

    todolist.vue updated

--- vue_this/src/components/TodoList.vue
+++ vue_this/src/components/TodoList.vue
@@ -1,8 +1,24 @@
-<script lang="ts" setup> import ListItem from './ListItem.vue' </script>
+<script lang="ts" setup> 
+    import ListItem from './ListItem.vue' 
+    type Item = {
+        title: string,
+        checked?: boolean
+    }
+    const items: listItems: Item[] = [
+        { title: 'Buy milk', checked: false },
+        { title: 'Buy eggs', checked: true }
+    ]
+</script>
+
 <template>
     <ul>
-        <ListItem :is-checked="false">
-            Buy milk
+        <li
+            :key='key'
+            v-for='(item, key) in items'
+        >
+        <ListItem :is-checked='item.checked'>
+            {{ item.title }}
         </ListItem>
+
     </ul>
 </template>
